461f8abe6f1ef06166b751799da539c5353fc9ad
arockiaanandraj <arockiaanandraj@gmail.com>
2019-11-19 23:18:28 -0800

    Added Ribbon

--- src/main/java/com/payroll/microservices/employeepayrollservice/EmployeeService.java
+++ src/main/java/com/payroll/microservices/employeepayrollservice/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.payroll.microservices.employeepayrollservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "employee-service", url = "localhost:8080")
+//@FeignClient(name = "employee-service", url = "localhost:8001")
+@FeignClient(name = "employee-service")
+@RibbonClient(name = "employee-service")
 public interface EmployeeService {
 
 	@GetMapping("/employee/{empId}")
