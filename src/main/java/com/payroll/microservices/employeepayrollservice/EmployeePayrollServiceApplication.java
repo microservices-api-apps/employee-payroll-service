a2deba0b78be527b70badd5d44808d4c749cc65d
arockiaanandraj <arockiaanandraj@gmail.com>
2019-11-20 03:29:17 -0800

    Added Eureka features

--- src/main/java/com/payroll/microservices/employeepayrollservice/EmployeePayrollServiceApplication.java
+++ src/main/java/com/payroll/microservices/employeepayrollservice/EmployeePayrollServiceApplication.java
@@ -2,10 +2,12 @@ package com.payroll.microservices.employeepayrollservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EmployeePayrollServiceApplication {
 
 	public static void main(String[] args) {
